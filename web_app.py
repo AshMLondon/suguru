7a2cc1d918105b9fe73d02d1c6d5edf69ea7c7f0
AshMLondon <98660851+AshMLondon@users.noreply.github.com>
2022-05-01 14:42:10 +0100

    added web_app using Flask to get that going

--- web_app.py
+++ web_app.py
@@ -1,26 +1,27 @@
-#procedure calling test
-#just checking if i can call procedures from other files ok
+## Web App
+## Let's try to get this stuff working on the web
 
+from flask import Flask
 import gridgenerate as gridgen
 from time import time
 
 
-def main():
-    print ("procedure calling test")
+## Start Flask Running
+## note to work properly the app.run is at the end after the definitions..
+app = Flask(__name__)
+print ("flask should be running...")
 
-    #let's start with the random colour one which has no inputs
-    #from gridgen  import random_colour, gen_predet_shapes, translated_shapes
 
 
-    print (gridgen.random_colour())
-
+@app.route("/")
+@app.route("/index")
+def generate_some_grids():
     #now let's try generating a grid
     #global initial variables
     gridgen.num_rows=10
     gridgen.num_cols=10
     gridgen.verbose=False
     gridgen.display_build=False
-
 
     # grid = np.zeros((gridgen.num_rows, gridgen.num_cols), dtype=int)
     # grid_shapes = np.zeros((gridgen.num_rows, gridgen.num_cols), dtype=int)
@@ -36,5 +37,14 @@ def main():
     print(gridgen.grid_shapes)
     print ("elapsed = ",elapsed)
 
+    return("finished")
+
+
+
+
 if __name__ == '__main__':
-    main()
+    app.run()
+
+
+
+
